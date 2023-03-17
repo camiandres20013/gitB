67d58d6847c0751cf3d5f8d7c39d8fb044862744
camiandres20013 <camiandres20013@gmail.com>
2023-03-17 06:15:52 -0500

    ejercicios_clase_16/03

---
+++ ejercicios_varios.py
@@ -0,0 +1,50 @@
+#tipos de datos
+a = 2 #numero entero
+b = 2.1 #numero flotante
+c = 4+3j # numero complejo
+d = False # Dato de tipo booleano
+e = True # Dato de tipo booleano
+f= "cadena"
+
+print(type(a),a,", Este es el entero")
+print(type(b),b,", Este es el flotante")
+print(type(c),c,", Este es el complejo")
+print(type(d),d,", Este es booleano")
+print(type(e),e,", Este es booleano")
+print(type(f),f,", Este es una cadena de caracteres")
+
+#operadores aritmeticos
+#   potencias
+import math
+print(2**8)
+print(pow(2,8))
+print(math.pow(2,8))
+#   #Operador Modulo %
+var = 10%2
+print(var)
+#   suma resta multiplicaion 
+z = (2+3)*4
+print(z)
+
+#operadores logicos 
+#    AND
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+#    or
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+#    not
+print(not(True))
+print(not(False))
+
+#operadores racionales
+print(2>3)
+print(2<3)
+print(2>=3)
+print(2<=3)
+print(2==3)
+print(2!=3) #diferentes
